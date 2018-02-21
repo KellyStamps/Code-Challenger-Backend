7567222ece77d3f3eb83902dd65a1c4d6a940bc9
KellyStamps <kellydsample@gmail.com>
2018-02-21 12:03:49 -0500

    configured cors and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:new, :create, :show, :index, :update, :edit]
+      resources :challenges, only: [:new, :create, :show, :index, :destroy]
+      resources :user_challenges, only: [:new, :create, :destroy]
+      resources :friendships, only: [:new, :create, :destroy]
+    end
+  end
+
 end
