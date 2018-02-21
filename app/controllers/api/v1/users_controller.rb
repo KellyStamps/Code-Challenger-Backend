50c26a67a3e17205e47dcb81d816d097d9e94f6e
KellyStamps <kellydsample@gmail.com>
2018-02-21 16:29:57 -0500

    configured user create method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,30 @@
 class Api::V1::UsersController < ApplicationController
 
-  def new
-  end
-
   def create
+    user = User.new(username: params[:username], password: params[:password])
+
+    if user.save
+      render json: {
+        username: user.username,
+        cake_day: user.created_at,
+        id: user.id
+      }
+    else
+      render json: {
+        error: 'Something went horribly wrong'
+      }
+    end
   end
 
   def show
+    user = User.find_by(username: params[:id])
   end
 
   def index
+    users = User.all
+    render json: {
+      users: users 
+    }
   end
 
   def edit
