ac5378072ea92cb54449dfa91bce4e4c819a2b1a
KellyStamps <kellydsample@gmail.com>
2018-02-26 15:36:05 -0500

    can persist user favorites from front end

--- app/controllers/api/v1/user_challenges_controller.rb
+++ app/controllers/api/v1/user_challenges_controller.rb
@@ -1,12 +1,27 @@
 class Api::V1::UserChallengesController < ApplicationController
 
-  def new
-  end
-
   def create
+    new_user_challenge = UserChallenge.new(user_challenge_params)
+
+    if new_user_challenge.save
+      render json: {
+        user_id: new_user_challenge.user_id,
+        challenge_id: new_user_challenge.challenge_id,
+        created_at: new_user_challenge.created_at.strftime('%A, %B %d, %Y')
+      }
+    else
+      render json: {
+        error: 'Something went horribly wrong'
+      }
+    end
   end
 
   def destroy
+  end
+
+  private
+  def user_challenge_params
+    params.require(:user_challenge).permit(:user_id, :challenge_id)
   end
 
 end
