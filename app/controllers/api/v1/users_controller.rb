1a9a9276547ae86735b08aa4c41e7d0780f17d19
KellyStamps <kellydsample@gmail.com>
2018-02-28 14:04:37 -0500

    changed json response for update user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -46,13 +46,20 @@ class Api::V1::UsersController < ApplicationController
 
   def update
     user = User.find(params[:id])
+
     if !params[:bio].empty?
       user.update(bio: params[:bio])
     end
-    if !params[:username].empty?
-      user.update(username: params[:username])
-    end
-    render json: {user: user}
+
+    favorites = user.user_challenges.map { |c| {id: c.id, challenge: c.challenge, completed: c.completed, git_link: c.git_link}}
+    
+    render json: {
+      username: user.username,
+      cake_day: user.created_at.strftime('%A, %B %d, %Y'),
+      id: user.id,
+      bio: user.bio,
+      favorites: favorites
+    }
   end
 
   # private
