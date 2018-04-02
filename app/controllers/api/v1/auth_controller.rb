6eddae596592e9e52445840a0746afb4526fa424
KellyStamps <kellydsample@gmail.com>
2018-04-02 10:54:19 -0400

    make adjustments for auth

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,16 +1,19 @@
-class AuthController < ApplicationController
+class Api::V1::AuthController < ApplicationController
 
   def show
     user = find_current_user
-    projects = user.user_challenges.map { |uc| {id: uc.id, title: uc.challenge.content, project: uc}
+
+    projects = user.user_challenges.map {|uc| {id: uc.id, title: uc.challenge.content, project: uc}}
+
     render json: {user: {username: user.username, id: user.id, bio: user.bio, projects: projects}}
   end
 
   def create
-    user = User.find(params[:username])
+    user = User.find_by(username: params[:username])
     if user.present? && user.authenticate(params[:password])
       jwt = encode(id: user.id)
-      render json: {username: user.username, id: user.id, jwt: jwt}
+      projects = user.user_challenges.map {|uc| {id: uc.id, title: uc.challenge.content, project: uc}}
+      render json: {user: {username: user.username, id: user.id, bio: user.bio, projects: projects}, jwt: jwt}
     else
       render json: {error: "Something went terribly wrong"}, status: 404
     end
