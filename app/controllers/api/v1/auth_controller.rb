0db9498415f19f576a700a37b1f2f69a9e793628
KellyStamps <kellydsample@gmail.com>
2018-04-02 11:32:36 -0400

    more tweaks to auth

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,19 +1,58 @@
 class Api::V1::AuthController < ApplicationController
+  before_action :authorize_user!, only: [:show]
 
   def show
+
     user = find_current_user
 
-    projects = user.user_challenges.map {|uc| {id: uc.id, title: uc.challenge.content, project: uc}}
+    if find_current_user.user_challenges
+      favorites = find_current_user.user_challenges.map { |c| {id: c.id, challenge: c.challenge, completed: c.completed, git_link: c.git_link, live_link: c.live_link}}
+    else
+      favorites = []
+    end
 
-    render json: {user: {username: user.username, id: user.id, bio: user.bio, projects: projects}}
+    if find_current_user.friendships
+      friends = find_current_user.friendships.map {|fr| {id: fr.id, friend: fr.friend}}
+    else
+      friends = []
+    end
+
+    render json: {
+      username: find_current_user.username,
+      cake_day: find_current_user.created_at.strftime('%A, %B %d, %Y'),
+      id: find_current_user.id,
+      bio: find_current_user.bio,
+      favorites: favorites,
+      friends: friends
+    }
   end
 
   def create
     user = User.find_by(username: params[:username])
     if user.present? && user.authenticate(params[:password])
       jwt = encode(id: user.id)
-      projects = user.user_challenges.map {|uc| {id: uc.id, title: uc.challenge.content, project: uc}}
-      render json: {user: {username: user.username, id: user.id, bio: user.bio, projects: projects}, jwt: jwt}
+
+      if user.user_challenges
+        favorites = user.user_challenges.map { |c| {id: c.id, challenge: c.challenge, completed: c.completed, git_link: c.git_link, live_link: c.live_link}}
+      else
+        favorites = []
+      end
+
+      if user.friendships
+        friends = user.friendships.map {|fr| {id: fr.id, friend: fr.friend}}
+      else
+        friends = []
+      end
+
+      render json: { user: {
+        username: user.username,
+        cake_day: user.created_at.strftime('%A, %B %d, %Y'),
+        id: user.id,
+        bio: user.bio,
+        favorites: favorites,
+        friends: friends},
+        jwt: jwt
+      }
     else
       render json: {error: "Something went terribly wrong"}, status: 404
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,35 @@
 class ApplicationController < ActionController::API
 
+  private
+
   def encode payload
     JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
   end
 
   def decode
     if request.headers['Authorization']
-      JWT.decode(request.headers['Authorization'], Rails.application.secrets.secret_key_base, true, 'HS256')
-    else
-      [{}]
-    end
+        begin
+          JWT.decode(request.headers['Authorization'], Rails.application.secrets.secret_key_base, true, {algorithm: "HS256"})
+        rescue JWT::DecodeError
+          return [{}]
+        end
+      else
+        [{}]
+      end
   end
 
+  def authorize_user!
+      if !find_current_user.present?
+        render json: {error: 'No user id present'}
+      end
+    end
+
   def current_user
-    !!@user ||= User.find_by(id: decode.first[:id])
+    !!@current_user ||= User.find_by(id: decode.first[:id])
   end
 
   def find_current_user
-    @user ||= User.find_by(id: decode.first[:id])
+    @current_user ||= User.find_by(id: decode.first['id'])
   end
 
 end
